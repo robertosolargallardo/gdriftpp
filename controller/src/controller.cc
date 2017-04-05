3f007b370e3239e63eea9a54052b68de5917b681
vsepulve <vsepulve@dcc.uchile.cl>
2017-04-05 14:33:23 -0300

    Minor adjustments to controller.cc code, std::mutex instead of Semaphore.

--- controller/src/controller.cc
+++ controller/src/controller.cc
@@ -6,6 +6,9 @@
 #include <Logger.h>
 #include <Controller.h>
 
+#include <thread>
+#include <mutex>
+
 using namespace std;
 using namespace restbed;
 using namespace boost::property_tree;
@@ -13,44 +16,57 @@ using namespace boost::property_tree;
 random_device seed;
 mt19937 rng(seed());
 
-shared_ptr<util::Semaphore>  semaphore;
+//shared_ptr<util::Semaphore>  semaphore;
+std::mutex internal_mutex;
 shared_ptr<Controller> controller;
 
 void run(boost::property_tree::ptree _frequest){
-   semaphore->lock();
+//	semaphore->lock();
+	lock_guard<mutex> lock(internal_mutex);
 	controller->run(_frequest);
-   semaphore->unlock();
+//	semaphore->unlock();
 }
-int main(int argc,char** argv){
-	if(argc<3){
-   	cerr << "Error::Hosts File and/or ControllerID not Specified" << endl;
+
+int main(int argc, char **argv){
+	if(argc < 3){
+		cerr<<"Error::Hosts File and/or ControllerID not Specified"<<endl;
 		exit(EXIT_FAILURE);
 	}
 	
 	ptree fhosts;
-	read_json(argv[1],fhosts);
-
-	uint32_t id=atoi(argv[2]);
-   unsigned MAX_THREADS=std::thread::hardware_concurrency();
-   semaphore=make_shared<util::Semaphore>(MAX_THREADS);
-   //semaphore=make_shared<util::Semaphore>(1);
-	controller=make_shared<Controller>(fhosts,id);
-
-	auto myself=std::find_if(fhosts.get_child("controller").begin(),fhosts.get_child("controller").end(),[&id](const pair<const basic_string<char>,basic_ptree<basic_string<char>,basic_string<char>>> &p)->bool{return p.second.get<uint32_t>("id")==id;});
-
-   auto resource=make_shared<Resource>();
-   resource->set_path(myself->second.get<string>("resource"));
-   resource->set_method_handler("OPTIONS",method::options);
-   resource->set_method_handler("POST",method::post<run>);
-
-   auto settings=make_shared<Settings>();
-   settings->set_port(myself->second.get<int>("port"));
-   settings->set_default_header("Connection","close");
-
-   Service service;
-   service.publish(resource);
+	read_json(argv[1], fhosts);
+	uint32_t id = atoi(argv[2]);
+	
+//	unsigned MAX_THREADS = std::thread::hardware_concurrency();
+//	semaphore = make_shared<util::Semaphore>(MAX_THREADS);
+//	semaphore = make_shared<util::Semaphore>(1);
+	
+	controller = make_shared<Controller>(fhosts, id);
+	
+	auto myself = std::find_if(
+						fhosts.get_child("controller").begin(), 
+						fhosts.get_child("controller").end(), 
+						[&id](const pair<const basic_string<char>, 
+										basic_ptree<basic_string<char>, basic_string<char>>> &p)->bool{return p.second.get<uint32_t>("id")==id;}
+						);
+	
+	auto resource = make_shared<Resource>();
+	resource->set_path(myself->second.get<string>("resource"));
+	resource->set_method_handler("OPTIONS", method::options);
+	resource->set_method_handler("POST", method::post<run>);
+	
+	auto settings = make_shared<Settings>();
+	settings->set_port(myself->second.get<int>("port"));
+	settings->set_default_header("Connection", "close");
+	
+	Service service;
+	service.publish(resource);
 	service.set_logger(make_shared<CustomLogger>());
-   service.start(settings);
-
-   return(EXIT_SUCCESS);
+	service.start(settings);
+	
+	return(EXIT_SUCCESS);
 }
+
+
+
+
