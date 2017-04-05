0f43c1bfca13029776d7a2ea06b5d6dcd4ca856f
vsepulve <vsepulve@dcc.uchile.cl>
2017-04-05 14:54:45 -0300

    Global mongocxx::instance added to scheduler/src/scheduler.cc

--- scheduler/src/scheduler.cc
+++ scheduler/src/scheduler.cc
@@ -6,17 +6,22 @@
 #include <Logger.h>
 #include <Method.h>
 
+#include <mongocxx/instance.hpp>
+
 #include <Scheduler.h>
 
 random_device seed;
 mt19937 rng(seed());
+mongocxx::instance inst{};
+
 shared_ptr<Scheduler> scheduler;
 
 void run(boost::property_tree::ptree _frequest){
 	scheduler->run(_frequest);
 }
+
 int main(int argc,char** argv){
-	
+
 	/*boost::property_tree::ptree fhosts;
 	read_json(argv[1],fhosts);
 
@@ -24,32 +29,32 @@ int main(int argc,char** argv){
 	read_json(argv[2],frequest);
 
 	scheduler=make_shared<Scheduler>(fhosts);
-	
+
 	scheduler->run(frequest);*/
 
-	if(argc<2){
-   	cerr << "Error::Hosts File not Specified" << endl;
+	if(argc < 2){
+		cerr << "Error::Hosts File not Specified" << endl;
 		exit(EXIT_FAILURE);
 	}
-	
+
 	boost::property_tree::ptree fhosts;
-	read_json(argv[1],fhosts);
+	read_json(argv[1], fhosts);
 
 	scheduler=make_shared<Scheduler>(fhosts);
 
-   auto resource=make_shared<Resource>();
-   resource->set_path(fhosts.get<string>("scheduler.resource"));
-   resource->set_method_handler("OPTIONS",method::options);
-   resource->set_method_handler("POST",method::post<run>);
+	auto resource=make_shared<Resource>();
+	resource->set_path(fhosts.get<string>("scheduler.resource"));
+	resource->set_method_handler("OPTIONS", method::options);
+	resource->set_method_handler("POST", method::post<run>);
 
-   auto settings=make_shared<Settings>();
-   settings->set_port(fhosts.get<int>("scheduler.port"));
-   settings->set_default_header("Connection","close");
+	auto settings = make_shared<Settings>();
+	settings->set_port(fhosts.get<int>("scheduler.port"));
+	settings->set_default_header("Connection", "close");
 
-   Service service;
-   service.publish(resource);
+	Service service;
+	service.publish(resource);
 	service.set_logger(make_shared<CustomLogger>());
-   service.start(settings);
+	service.start(settings);
 
-   return(EXIT_SUCCESS);
+	return(EXIT_SUCCESS);
 }
