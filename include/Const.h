6083f584d2e12490c5defa83ac5646eb654b72ae
vsepulve <vsepulve@dcc.uchile.cl>
2017-04-18 13:29:10 -0300

    Values of the simulation decreased for small scale tests.

--- include/Const.h
+++ include/Const.h
@@ -1,7 +1,7 @@
 #ifndef _CONST_H_
 #define _CONST_H_
-#define BATCH_LENGTH 32
-//#define BATCH_LENGTH 8
+//#define BATCH_LENGTH 32
+#define BATCH_LENGTH 8
 #define MAX_DIST 		1024.0
 #define PERCENT  		1.0
 
