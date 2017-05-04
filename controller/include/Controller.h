4edaae7abe4947a4e0065f8892130a37f679daf4
vsepulve <vsepulve@dcc.uchile.cl>
2017-05-04 15:35:25 -0300

    Functional version of the new threads model for testing.

--- controller/include/Controller.h
+++ controller/include/Controller.h
@@ -15,7 +15,6 @@ class Controller : public Node{
 private:
 	uint32_t _id;
 	// Total de errores encontrados
-	static unsigned int total_errores;
 
 public:
 	Controller(const boost::property_tree::ptree&, const uint32_t&);
@@ -24,24 +23,52 @@ public:
 	~Controller(void);
 	
 	// Marcador para iniciar los threads procesadores
-	// El programa debe iniciarlos con "Controller::thread_started = Controller::startThreads(n_threads, queue, mutex);" antes del main
+	// El programa debe iniciarlos con "bool Controller::thread_started = Controller::startThreads(n_threads, queue, mutex);" antes del main
 	static bool thread_started;
 	// Proceso de inicio de los threads
-	static bool startThreads(unsigned int n_threads, list<shared_ptr<boost::property_tree::ptree>> *work_list, std::mutex *list_mutex);
+	static bool startThreads(unsigned int n_threads, list<shared_ptr<boost::property_tree::ptree>> *work_list, std::mutex *list_mutex, const boost::property_tree::ptree &fhosts);
 	
 class ThreadData{
 	public:
 		unsigned int id;
 		list<shared_ptr<boost::property_tree::ptree>> *work_list;
 		std::mutex *list_mutex;
+		string analyzer_host;
+		string analyzer_port;
+		string analyzer_resource;
 		
 		ThreadData(){
 			id = 0;
 			work_list = NULL;
 			list_mutex = NULL;
+			analyzer_host = "";
+			analyzer_port = "";
+			analyzer_resource = "";
 		}
 		~ThreadData(){}
 };
+	static unsigned int total_errores;
+	static vector<Controller::ThreadData> threads_data;
 	
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #endif
--- controller/src/Controller.cc
+++ controller/src/Controller.cc
@@ -1,6 +1,7 @@
 #include "Controller.h"
 
 unsigned int Controller::total_errores = 0;
+vector<Controller::ThreadData> Controller::threads_data;
 
 Controller::Controller(const boost::property_tree::ptree &_fhosts, const uint32_t &_id) : Node(_fhosts) {
 	this->_id = _id;
@@ -24,14 +25,19 @@ boost::property_tree::ptree indices(map<string,Sample*> _samples){
 //void controller_thread(unsigned int id, list<shared_ptr<boost::property_tree::ptree>> *work_list, std::mutex *list_mutex){
 bool controller_thread(Controller::ThreadData *data){
 	
-	unsigned int sleep_time = 1;
+	data->list_mutex->lock();
+	cout<<"controller_thread["<<data->id<<"] - Inicio\n";
+	data->list_mutex->unlock();
+	
+	unsigned int sleep_time = 3;
 	shared_ptr<boost::property_tree::ptree> ptr_json;
 	
 	while(true){
 		data->list_mutex->lock();
+		cout<<"controller_thread["<<data->id<<"] - Largo de lista: "<<data->work_list->size()<<"\n";
 		if(data->work_list->empty()){
-			data->list_mutex->unlock();
 			cout<<"controller_thread["<<data->id<<"] - Durmiendo\n";
+			data->list_mutex->unlock();
 			std::this_thread::sleep_for (std::chrono::seconds(sleep_time));
 			continue;
 		}
@@ -42,28 +48,89 @@ bool controller_thread(Controller::ThreadData *data){
 		}
 		
 		// Procesar el json
-		cout<<"controller_thread["<<data->id<<"] - Procesando json\n";
+//		data->list_mutex->lock();
+//		cout<<"controller_thread["<<data->id<<"] - Procesando json\n";
+//		data->list_mutex->unlock();
 		
-		std::stringstream ss;
-		write_json(ss, *ptr_json);
-		cout<<ss.str()<<"\n";
+		boost::property_tree::ptree _frequest = *ptr_json;
 		
-		cout<<"controller_thread["<<data->id<<"] - -----------------------\n";
+		cout<<"Controller::run - Inicio (sim "<<_frequest.get<string>("id")<<", scenario: "<<_frequest.get_child("scenario").get<uint32_t>("id")<<", run "<<_frequest.get<string>("run")<<", batch "<<_frequest.get<string>("batch")<<")\n";
+	
+		boost::property_tree::ptree findividual = _frequest.get_child("individual");
+		boost::property_tree::ptree fscenario = _frequest.get_child("scenario");
+
+		boost::property_tree::ptree fresponse;
+		boost::property_tree::ptree fprior;
+		boost::property_tree::ptree fposterior;
+
+		fresponse.put("id", _frequest.get<string>("id"));
+		fresponse.put("run", _frequest.get<string>("run"));
+		fresponse.put("batch", _frequest.get<string>("batch"));
+		fresponse.put("type","simulated");
+		fresponse.put("max-number-of-simulations", _frequest.get<string>("max-number-of-simulations"));
+	
+		fresponse.put("feedback", _frequest.get<uint32_t>("feedback"));
+		fresponse.put("simulations-per-feedback", _frequest.get<uint32_t>("simulations-per-feedback"));
+	
+//		cout<<"Controller::run - Creando Simulatior\n";
+		Simulator sim(_frequest);
+	
+		//fprior.push_back(std::make_pair("populations", indices(sim.populations())));
+	
+		//printf("%s %s %s\n",_frequest.get<string>("id").c_str(), _frequest.get<string>("run").c_str(), _frequest.get<string>("batch").c_str());
+//		cout<<"Controller::run - Lanzando sim.run...\n";
+		sim.run();
+//		cout<<"Controller::run - Ok\n";
+	
+		if( sim.detectedErrors() == 0 ){
+			fposterior.push_back(std::make_pair("populations", indices(sim.samples())));
+			fresponse.push_back(make_pair("prior", fprior));
+			fresponse.push_back(make_pair("posterior", fposterior));
+			fresponse.push_back(make_pair("individual", findividual));
+			fresponse.push_back(make_pair("scenario", fscenario));
+		}
+		else{
+			
+			data->list_mutex->lock();
+			cout<<"Controller::run - Error durante simulacion, resultados no usables.\n";
+			data->list_mutex->unlock();
+			
+			fresponse.put("error", sim.detectedErrors());
+			++Controller::total_errores;
+		}
+		comm::send(data->analyzer_host, data->analyzer_port, data->analyzer_resource, fresponse);
+		
+		// cout<<"Controller::run - Fin (sim "<<_frequest.get<string>("id")<<", run "<<_frequest.get<string>("run")<<", batch "<<_frequest.get<string>("batch")<<")\n";
+		
+//		data->list_mutex->lock();
+//		cout<<"Controller::run - Fin (total_errores: "<<Controller::total_errores<<")\n";
+//		data->list_mutex->unlock();
+		
+		
 		
 		
 	}
 	
 }
 
-bool Controller::startThreads(unsigned int n_threads, list<shared_ptr<boost::property_tree::ptree>> *work_list, std::mutex *list_mutex){
+bool Controller::startThreads(unsigned int n_threads, list<shared_ptr<boost::property_tree::ptree>> *work_list, std::mutex *list_mutex, const boost::property_tree::ptree &fhosts){
 	
 	cout<<"Controller::startThreads - Preparando datos\n";
-	ThreadData threads_data[n_threads];
+	// Notar que, dado que los threads son detached, almaceno sus datos en una variable estatica de la clase 
+//	ThreadData threads_data[n_threads];
 	for(unsigned int i = 0; i < n_threads; ++i){
-		threads_data[i].id = i;
-		threads_data[i].work_list = work_list;
-		threads_data[i].list_mutex = list_mutex;
+		Controller::ThreadData data;
+		data.id = i;
+		data.work_list = work_list;
+		data.list_mutex = list_mutex;
 		
+//		comm::send(this->_fhosts.get<string>("analyzer.host"), this->_fhosts.get<string>("analyzer.port"), this->_fhosts.get<string>("analyzer.resource"), fresponse);
+		
+		data.analyzer_host = fhosts.get<string>("analyzer.host");
+		data.analyzer_port = fhosts.get<string>("analyzer.port");
+		data.analyzer_resource = fhosts.get<string>("analyzer.resource");
+		
+		threads_data.push_back(data);
 	}
 	
 	cout<<"Controller::startThreads - Lanzando threads\n";
