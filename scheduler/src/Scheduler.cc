1f2c16fcc04b0a9954ef4931e983e0494f5e2833
vsepulve <vsepulve@dcc.uchile.cl>
2017-06-15 16:50:19 -0300

    Security verifications added to the scheduler.

--- scheduler/src/Scheduler.cc
+++ scheduler/src/Scheduler.cc
@@ -11,13 +11,30 @@ Scheduler::~Scheduler(void){
 }
 
 boost::property_tree::ptree Scheduler::run(boost::property_tree::ptree &_frequest){
-	uint32_t id = _frequest.get<uint32_t>("id");
-	string type = _frequest.get<string>("type");
+
+	boost::optional<boost::property_tree::ptree&> test_child;
+	uint32_t id = 0;
+	string type = "unknown";
+	
+	test_child = _frequest.get_child_optional("id");
+	if( test_child ){
+		id = _frequest.get<uint32_t>("id");
+	}
+	
+	test_child = _frequest.get_child_optional("type");
+	if( test_child ){
+		type = _frequest.get<string>("type");
+	}
+	
 	cout<<"Scheduler::run - Inicio (id: "<<id<<", type: "<<type<<")\n";
 	
 	switch(util::hash(type)){
 		case PAUSE:{
 			cout<<"Scheduler::run - PAUSE\n";
+			if( _settings.find(id) == _settings.end() ){
+				cout<<"Scheduler::run - Simulacion NO encontrada.\n";
+				break;
+			}
 			_mongo->setStatus(_fhosts.get<string>("database.name"), _fhosts.get<string>("database.collections.settings"), id, "paused");
 			this->_settings[id]->pause = true;
 			break;
@@ -35,6 +52,10 @@ boost::property_tree::ptree Scheduler::run(boost::property_tree::ptree &_freques
 		}
 		case CANCEL:{
 			cout<<"Scheduler::run - CANCEL\n";
+			if( _settings.find(id) == _settings.end() ){
+				cout<<"Scheduler::run - Simulacion NO encontrada.\n";
+				break;
+			}
 			_mongo->setStatus(_fhosts.get<string>("database.name"), _fhosts.get<string>("database.collections.settings"), id, "canceled");
 			// Lo primero es activar la marca de cancelacion (o quitar la marca de continuar)
 			// Notar que _settings[id] probablemente estara corriendo (send), y NO PUEDE eliminarse mientras eso siga activo
@@ -53,7 +74,6 @@ boost::property_tree::ptree Scheduler::run(boost::property_tree::ptree &_freques
 			cout<<"Scheduler::run - INIT\n";
 			// Notar que aqui NO se puede actualizar porque aun no se ha guardado, pero lo puedo agregar como el feedback
 //			_mongo->setStatus(_fhosts.get<string>("database.name"), _fhosts.get<string>("database.collections.settings"), id, "running");
-			boost::optional<boost::property_tree::ptree&> test_child;
 			
 			// Agregar cualquier informacion al json antes de guardarlo en la BD
 			
@@ -93,13 +113,21 @@ boost::property_tree::ptree Scheduler::run(boost::property_tree::ptree &_freques
 			break;
 		}
 		case CONTINUE:{
-			cout<<"Scheduler::run - CONTINUE ("<<this->_settings[id]->_training_size<<")\n";
+			cout<<"Scheduler::run - CONTINUE\n";
+			if( _settings.find(id) == _settings.end() ){
+				cout<<"Scheduler::run - Simulacion NO encontrada.\n";
+				break;
+			}
 //			this->_settings[id]->send(BATCH_LENGTH, this->_fhosts);
 			this->_settings[id]->send(this->_settings[id]->_training_size, this->_fhosts);
 			break;
 		}
 		case RELOAD:{
 			cout<<"Scheduler::run - RELOAD\n";
+			if( _settings.find(id) == _settings.end() ){
+				cout<<"Scheduler::run - Simulacion NO encontrada.\n";
+				break;
+			}
 			// Aqui hay que guardar _frequest nuevamente en settings, pues trae nuevos parametros para el propio scheduler
 			// Tambien hay que recrear settings (con los nuevos parametros)
 			// Hay que asegurar entonces, que en reload el analizer envie el ptree de settings !!!
@@ -130,6 +158,10 @@ boost::property_tree::ptree Scheduler::run(boost::property_tree::ptree &_freques
 		}
 		case FINALIZE:{
 			cout<<"Scheduler::run - FINALIZE\n";
+			if( _settings.find(id) == _settings.end() ){
+				cout<<"Scheduler::run - Simulacion NO encontrada.\n";
+				break;
+			}
 			_mongo->setStatus(_fhosts.get<string>("database.name"), _fhosts.get<string>("database.collections.settings"), id, "ended");
 			this->_semaphore->lock();
 			this->_settings.erase(this->_settings.find(id));
@@ -138,7 +170,8 @@ boost::property_tree::ptree Scheduler::run(boost::property_tree::ptree &_freques
 		}
 		default:{
 			cerr<< "Error::Unknown Scheduling Type " << type << endl;
-			exit(EXIT_FAILURE);
+			break;
+//			exit(EXIT_FAILURE);
 		}
 	}
 	
